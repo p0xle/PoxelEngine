b40c0bac5d468cf68846df02d2dc05a08b89c69b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 23:54:51 +0200

    Added Player Class

---
+++ ConsoleApp1/Demo Game/Models/Player.cs
@@ -0,0 +1,38 @@
+﻿using EngineTest.Demo_Game.Components;
+using EngineTest.Models;
+using PoxelEngine.Components;
+using PoxelEngine.Models;
+
+namespace EngineTest.Demo_Game
+{
+    public class Player : GameObject
+    {
+        protected Player(Transform transform, string tag, float playerSpeed = 3f) : base(tag, transform)
+        {
+            this.Speed = playerSpeed;
+        }
+
+        public static Player Create(Transform transform, string tag, string path, float playerSpeed = 3f)
+        {
+            var player = new Player(transform, tag, playerSpeed);
+            return player.Initialize(path) ? player : null;
+        }
+
+        protected bool Initialize(string path)
+        {
+            if (!this.LoadComponent(new SpriteComponent(this, path)))
+                return false;
+            if (!this.LoadComponent(new MoveComponent(this, this.Speed, this.Directions)))
+                return false;
+            if (!this.LoadComponent(new InputComponent(this, this.Directions)))
+                return false;
+            if (!this.LoadComponent(new CoinComponent(this)))
+                return false;
+
+            return base.Initialize();
+        }
+
+        public float Speed;
+        public Directions Directions = new Directions();
+    }
+}
