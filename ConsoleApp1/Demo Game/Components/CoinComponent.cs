8e325bcc35a07e17b795e38c6fce09cfc7419f40
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 23:54:04 +0200

    Added CoinComponent

---
+++ ConsoleApp1/Demo Game/Components/CoinComponent.cs
@@ -0,0 +1,37 @@
+﻿using PoxelEngine.Components;
+using PoxelEngine.Models;
+
+namespace EngineTest.Demo_Game.Components
+{
+    public class CoinComponent : Component
+    {
+        public CoinComponent(GameObject gameObject) : base(gameObject)
+        {
+        }
+
+        public int Coins { get; set; } = 0;
+
+        private readonly string coinTag = "Coin";
+
+        public override bool Init()
+        {
+            return true;
+        }
+
+        public override void Update()
+        {
+            var collisionComponent = this.GameObject?.GetComponent<CollisionComponent>();
+            var coin = collisionComponent?.IsColliding(this.coinTag);
+            if (coin != null)
+            {
+                coin.DestroySelf();
+                this.Coins--;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+
+        }
+    }
+}
