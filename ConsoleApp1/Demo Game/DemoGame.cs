3409002d633579d2dcf04addc1b1d64bea48fcd4
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 23:57:16 +0200

    DemoGame: - Added Changes from Engine - Added Scene Logic - Added Coin Logic and sort of a win condition - Changed Input Handling

--- ConsoleApp1/Demo Game/DemoGame.cs
+++ ConsoleApp1/Demo Game/DemoGame.cs
@@ -1,19 +1,15 @@
-﻿using PoxelEngine;
-using PoxelEngine.Models;
-using System;
-using System.Collections.Generic;
+﻿using EngineTest.Demo_Game.Components;
+using EngineTest.Demo_Game.Models;
+using PoxelEngine;
+using PoxelEngine.Utility;
 using System.Drawing;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EngineTest.Demo_Game
 {
     class DemoGame : Engine
     {
-        public DemoGame() : base(screenSize: new Vector2(windowWidth, windowHeight), title: title, isDebug: true) { }
+        public DemoGame() : base(screenSize: new Size(windowWidth, windowHeight), title: title, isDebug: false) { }
 
         private const string title = "Engine Demo Game";
 
@@ -22,43 +18,30 @@ namespace EngineTest.Demo_Game
 
         private const float zoomSpeed = .1f;
 
-        private const int groundSize = 64;
-
-        private Player Player;
-
-        private readonly string[,] Map =
-        {
-            {"g", "g", "g", "g", "g", "g", "g" },
-            {"g", ".", ".", ".", ".", "c", "g" },
-            {"g", ".", ".", ".", "g", "c", "g" },
-            {"g", ".", "g", "t", "g", "c", "g" },
-            {"g", "c", "g", "p", "g", "c", "g" },
-            {"g", "c", "g", ".", ".", "c", "g" },
-            {"g", "g", "g", "g", "g", "g", "g" },
-        };
+        private Scene CurrentScene { get; set; }
 
         public override void OnLoad()
         {
             this.BackgroundColor = Color.Black;
 
-            for (int i = 0; i < this.Map.GetLength(0); i++)
-            {
-                for (int j = 0; j < this.Map.GetLength(1); j++)
-                {
-                    if (this.Map[j, i] == "t")
-                    {
-                        Text.Create("Test", new Transform(new Vector2(i * groundSize, j * groundSize), new Vector2(100, 100), 10), "TestText",
-                            new FontOptions(family: new FontFamily("Comic Sans MS"), style: FontStyle.Underline));
-                    }
+            this.CurrentScene = this.CreateMainScene();
+            this.CurrentScene.Load();
+        }
 
-                    if (this.Map[j, i] == "p")
-                    {
-                        this.Player = Player.Create(transform: new Transform(new Vector2(i * groundSize, j * groundSize), new Vector2(48, 48)),
-                            tag: "Player", path: "player", playerSpeed: 5f);
-                    }
-                }
-            }
+        private Scene CreateMainScene()
+        {
+            string[,] map = {
+                {"g", "g", "g", "g", "g", "g", "g" },
+                {"g", ".", ".", ".", ".", "c", "g" },
+                {"g", ".", ".", ".", "g", "c", "g" },
+                {"g", ".", "g", "g", "g", "c", "g" },
+                {"g", "c", "g", "p", "g", "c", "g" },
+                {"g", "c", "g", ".", ".", "c", "g" },
+                {"g", "g", "g", "g", "g", "g", "g" },
+            };
 
+            var sceneSettings = new SceneSettings(map);
+            return new Scene(sceneSettings, "mainScene");
         }
 
         public override void OnDraw()
@@ -68,46 +51,43 @@ namespace EngineTest.Demo_Game
 
         public override void OnUpdate()
         {
+            var coinComponent = this.CurrentScene.Player?.GetComponent<CoinComponent>();
+            if (coinComponent != null)
+            {
+                if (coinComponent.Coins <= 0)
+                {
+                    Log.Info($"[{nameof(DemoGame)}] - All coins collected.. Congratulations!");
 
+                    // Todo: Load next Scene
+                }
+            }
         }
 
         public override void GetKeyDown(KeyEventArgs e)
         {
+            var inputComponent = this.CurrentScene.Player?.GetComponent<InputComponent>();
+            if (inputComponent != null)
+                inputComponent.HandleKeyDown(e.KeyCode);
+
             switch (e.KeyCode)
             {
-                case Keys.W:
-                    this.Player.Directions.Up = true;
-                    break;
-                case Keys.S:
-                    this.Player.Directions.Down = true;
-                    break;
-                case Keys.A:
-                    this.Player.Directions.Left = true;
-                    break;
-                case Keys.D:
-                    this.Player.Directions.Right = true;
+                default:
                     break;
             }
         }
 
         public override void GetKeyUp(KeyEventArgs e)
         {
+            var inputComponent = this.CurrentScene.Player?.GetComponent<InputComponent>();
+            if (inputComponent != null)
+                inputComponent.HandleKeyUp(e.KeyCode);
+
             switch (e.KeyCode)
             {
-                case Keys.W:
-                    this.Player.Directions.Up = false;
-                    break;
-                case Keys.S:
-                    this.Player.Directions.Down = false;
-                    break;
-                case Keys.A:
-                    this.Player.Directions.Left = false;
-                    break;
-                case Keys.D:
-                    this.Player.Directions.Right = false;
-                    break;
                 case Keys.Escape:
                     this.StopGame();
+                    break;
+                default:
                     break;
             }
         }
